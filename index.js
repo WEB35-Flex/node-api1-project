04501e5c7fd2c0f643347b7bc6aaf8c16bfeb990
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-11-04 21:24:37 -0800

    project completed

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ let users = [
   },
 ];
 
+let nextId = shortid.generate();
+
 server.get("/api/users", (req, res) => {
   if (!users) {
     res
@@ -38,6 +40,63 @@ server.get("/api/users/:id", (req, res) => {
     res
       .status(500)
       .json({ errorMessage: "The user information could not be retrieved " });
+  }
+});
+
+server.post("/api/users", (req, res) => {
+  const newUser = { id: nextId, name: req.body.name, bio: req.body.bio };
+
+  if (!newUser.name || !newUser.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else if (newUser.name || newUser.bio) {
+    users.push(newUser);
+    res.status(201).json(users);
+  } else {
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database",
+    });
+  }
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  const found = users.find((u) => u.id === id);
+
+  if (!found) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  } else if (found) {
+    users = users.filter((u) => u.id !== id);
+    res.status(200).json(found);
+  } else {
+    res.status(500).json({ errorMessage: "The user could not be removed" });
+  }
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const changes = req.body;
+  const id = req.params.id;
+  let found = users.find((u) => u.id === id);
+
+  if (!found) {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exists." });
+  } else if (!req.body.name || !req.body.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user" });
+  } else if (found) {
+    Object.assign(found, changes);
+
+    res.status(200).json(found);
+  } else {
+    res
+      .status(500)
+      .json({ errorMessage: "The user information could not be modified" });
   }
 });
 
